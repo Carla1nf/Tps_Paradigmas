a6752d495c56fd7e9d8b7dbf16bd6364e165c3ad
matteo <mattemusaa@gmail.com>
2023-08-25 19:22:47 -0300

    archivo testrun

--- TP1/city.hs
+++ TP1/city.hs
@@ -11,4 +11,3 @@ nameC (Cit nombre _) = nombre
 
 distanceC :: City -> City -> Float
 distanceC (Cit _ coordenadas1)(Cit _ coordenadas2) = difP coordenadas1 coordenadas2
-
--- TP1/tunel.hs
+++ TP1/tunel.hs
@@ -1,7 +1,7 @@
 module Tunel ( Tunel, newT, connectsT, usesT, delayT)
    where
 
-import City
+import City 
 import Point 
 import Quality 
 import Link
@@ -10,36 +10,18 @@ data Tunel = Tun [Link] deriving (Eq, Show)
 
 newT :: [Link] -> Tunel
 newT links = Tun links
+
 connectsT :: City -> City -> Tunel -> Bool -- inidca si este tunel conceta estas dos ciudades distintas
 connectsT ciudad_a_verificar_1 ciudad_a_verificar_2 (Tun []) = False
 connectsT ciudad_a_verificar_1 ciudad_a_verificar_2 (Tun _)| ciudad_a_verificar_1 == ciudad_a_verificar_2 = error "Mismas ciudades como input"
 connectsT ciudad_1 ciudad_2 (Tun (link:links)) | (connectsL ciudad_1 link || connectsL ciudad_2 link) && (connectsL ciudad_1 (last links) || connectsL ciudad_2 (last links)) = True
     | otherwise = False
+
 usesT :: Link -> Tunel -> Bool  -- indica si este tunel atraviesa ese link 
 usesT _ (Tun []) = False
 usesT link_a_verificar (Tun (link:links)) | link_a_verificar == link = True
     | otherwise = usesT link_a_verificar (Tun links)
+
 delayT :: Tunel -> Float -- la demora que sufre una conexion en este tunel
 delayT (Tun []) = 0
 delayT (Tun (link:links)) = delayL link + delayT (Tun links)
-
-madrid_location = newP 200 400
-madrid = newC "madrid" madrid_location
-paris_location = newP 1100 (-2100)
-paris = newC "paris" paris_location
-londres_location = newP 4000 2500
-londres = newC "londres" londres_location
-cable_titanio = newQ "Titanio" 4 0.1
-link_1 = newL paris madrid cable_titanio
-link_2 = newL madrid londres cable_titanio
-
-link_3 = newL londres barcelona cable_titanio
-
-barcelona_location = newP 100 200
-barcelona = newC "barcelona" barcelona_location
-
-cable_cobre = newQ "Cobre" 8 0.01
-
-link_4 = newL madrid barcelona cable_cobre
-
-tunel_1 = newT [link_1,link_2,link_3]
