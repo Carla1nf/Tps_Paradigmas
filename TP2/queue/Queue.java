e81e1089d25e6e31fe120290c4c2afb2339ba947
matteo <mattemusaa@gmail.com>
2023-09-01 15:01:02 -0300

    tp2

---
+++ TP2/queue/Queue.java
@@ -0,0 +1,34 @@
+package queue;
+
+public class Queue {
+
+  public boolean isEmpty() {
+		if this.Queue == [] {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+    
+	public Queue add( Object  cargo ) {
+		// TODO Auto-generated method stub
+		return this;
+	}
+    
+	public Object take() {
+    // TODO Auto-generated method stub
+		return null;
+	}
+    
+	public Object head() {
+		// TODO Auto-generated method stub
+    return null;
+	}
+
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
