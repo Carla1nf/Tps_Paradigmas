7497d5eb45a95e2535950804204aa182cc9f39cf
matteo <mattemusaa@gmail.com>
2023-09-10 19:43:20 -0300

    falta sacar los if

--- TP2/queue/Queue.java
+++ TP2/queue/Queue.java
@@ -1,45 +1,38 @@
 package queue;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class Queue {
 	
 	public static String QUEUE_IS_EMPTY = "Queue is empty";
-	public List<Object> queue = new ArrayList<>();
+	public ArrayList<Object> queueList = new ArrayList<Object>();
 
     public boolean isEmpty() {
-		return queue.isEmpty();
+		return queueList.isEmpty();
 	}
 
 	public Queue add( Object  cargo ) {
-		this.queue.add(cargo);
+		queueList.add(cargo);
 		return this;
 	}
 	
 	public Object take() {
-		if (this.queue.isEmpty()){
-			return new Error( QUEUE_IS_EMPTY);
+		if (this.queueList.isEmpty()){
+			throw new Error(QUEUE_IS_EMPTY);
 		}
-		Object lastAdded = this.queue.get(lastQueueObject());
-		this.queue.remove(lastQueueObject());
-		return lastAdded;
+		return this.queueList.remove(0);
 	}
 
 	public Object head() {
-		if (this.queue.isEmpty()){
-			return new Error( QUEUE_IS_EMPTY);
+		if (this.queueList.isEmpty()){
+			throw new Error(QUEUE_IS_EMPTY);
 		}
 		else{
-			return this.queue.get(0);
+			return this.queueList.get(0);
 		}
 	}
 
 	public int size() {
-		return this.queue.size();
-	}
-
-	private int lastQueueObject() {
-		return this.queue.size()-1;
+		return this.queueList.size();
 	}
 }
