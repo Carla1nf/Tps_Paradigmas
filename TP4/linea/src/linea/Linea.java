4d4d09532702377cfc2d355b2e4342bfff248d8b
matteo <mattemusaa@gmail.com>
2023-11-08 23:04:59 -0300

    mucho mejor

--- TP4/linea/src/linea/Linea.java
+++ TP4/linea/src/linea/Linea.java
@@ -1,21 +1,11 @@
 package linea;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
+import java.util.stream.IntStream;
 
 public class Linea {
-
-//    Se nos pide desarrollar la lógica de juego del '4 en línea'. (https://www.epasatiempos.es/juego-4-en-raya.php)
-//    El espacio de juego se define al iniciar, junto con la variante de triunfo.
-//    La variante de triunfo puede ser,
-//  - 'A' solo 4 en línea verticales u horizontales.
-//  - 'B' solo 4 en línea diagonales
-//  - 'C' 4 en línea en cualquier orientación.
-//    Inician el juego las Rojas y alternan los turnos a partir de ahí
-//    El juego puede terminar por triunfo o por empate. Una vez terminado no se puede seguir colocando fichas
-
+    public static final char EMPTY = '-';
     private int base;
     private int height;
     private ArrayList<ArrayList<Character>> board;
@@ -45,82 +35,56 @@ public class Linea {
     }
 
     public void board(){
-        for (int i = 0; i < this.height; i++) {
-            ArrayList<Character> row = new ArrayList<>();
-            for (int j = 0; j < this.base; j++) {
-                row.add('-');
-            }
-            this.board.add(row);
-        }
-    }
+        IntStream.range(0, this.height)
+                .forEach(i -> {
+                    ArrayList<Character> row = new ArrayList<>();
+                    IntStream.range(0, this.base)
+                            .forEach(j -> row.add(EMPTY));
+                    this.board.add(row);
+                });}
 
     public String show() {
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < this.height; i++) {
-            sb.append("|");
-            for (int j = 0; j < this.base; j++) {
-                sb.append(this.board.get(i).get(j));
-            }
-            sb.append("|\n");
-        }
-        return sb.toString();
+        return this.board.stream()
+                .map(row -> "|" + row.stream()
+                        .map(String::valueOf)
+                        .reduce("", String::concat) + "|\n")
+                .reduce("", String::concat);
     }
 
-
     public void playRedkAt(int position) {
-        if (position < 0 || position >= this.base) {
-            throw new RuntimeException("Invalid position");
-        }
-        if (this.board.get(0).get(position) != '-') {
-            throw new RuntimeException("Position already occupied");
-        }
-        if( turno instanceof TurnoAzul){
+        if (turno instanceof TurnoAzul){
             throw new RuntimeException("Turno de azules");
         }
-        int row = this.height - 1;
-        while (row >= 0 && this.board.get(row).get(position) != '-') {
-            row--;
-        }
+        int row = rowColumnCheck(position);
         this.board.get(row).set(position, this.redChar);
         turno = turno.alternarTurno();
         finished = estrategia.checkWin(this, this.redChar);
     }
 
-
     public void playBlueAt(int position) {
-        if (position < 0 || position >= this.base) {
-            throw new RuntimeException("Invalid position");
-        }
-        if (this.board.get(0).get(position) != '-') {
-            throw new RuntimeException("Position already occupied");
-        }
         if( turno instanceof TurnoRojas){
             throw new RuntimeException("Turno de rojas");
         }
-        int row = this.height - 1;
-        while (row >= 0 && this.board.get(row).get(position) != '-') {
-            row--;
-        }
+        int row = rowColumnCheck(position);
         this.board.get(row).set(position, this.blueChar);
         turno = turno.alternarTurno();
         finished = estrategia.checkWin(this, this.blueChar);
     }
 
+    public int rowColumnCheck(int position){
+        //buscar la primera fila vacia en la columna desde abajo hacia arriba
+
+        return IntStream.range(0, this.height)
+                .filter(i -> this.board.get(i).get(position) == EMPTY)
+                .reduce((a, b) -> b)
+                .orElseThrow(() -> new RuntimeException("Columna llena"));
+    }
+
     public boolean verticalWin(char color) {
-        for (int i = 0; i < this.base; i++) {
-            int count = 0;
-            for (int j = 0; j < this.height; j++) {
-                if (this.board.get(j).get(i) == color) {
-                    count++;
-                } else {
-                    count = 0;
-                }
-                if (count == 4) {
-                    return true;
-                }
-            }
-        }
-        return false;
+        return IntStream.range(0, this.base)
+                .anyMatch(column -> IntStream.range(0, this.height)
+                        .filter(row -> this.board.get(row).get(column) == color)
+                        .count() == 4);
     }
 
     public boolean horizontalWin(char color) {
@@ -132,34 +96,25 @@ public class Linea {
     }
 
     public boolean diagonalWinLeft(char color) {
-        for (int i = 0; i < this.height - 3; i++) {
-            for (int j = 0; j < this.base - 3; j++) {
-                if (this.board.get(i).get(j) == color &&
-                        this.board.get(i + 1).get(j + 1) == color &&
-                        this.board.get(i + 2).get(j + 2) == color &&
-                        this.board.get(i + 3).get(j + 3) == color) {
-                    return true;
-                }
-            }
-        }
-        return false;
+        return IntStream.range(0, this.height - 3)
+                .anyMatch(i -> IntStream.range(0, this.base - 3)
+                        .anyMatch(j -> this.board.get(i).get(j) == color &&
+                                this.board.get(i + 1).get(j + 1) == color &&
+                                this.board.get(i + 2).get(j + 2) == color &&
+                                this.board.get(i + 3).get(j + 3) == color));
     }
 
     public boolean diagonalWinRight(char color) {
-        for (int i = 0; i < this.height - 3; i++) {
-            for (int j = 3; j < this.base; j++) {
-                if (this.board.get(i).get(j) == color &&
-                        this.board.get(i + 1).get(j - 1) == color &&
-                        this.board.get(i + 2).get(j - 2) == color &&
-                        this.board.get(i + 3).get(j - 3) == color) {
-                    return true;
-                }
-            }
-        }
-        return false;
+        return IntStream.range(0, this.height - 3)
+                .anyMatch(i -> IntStream.range(3, this.base)
+                        .anyMatch(j -> this.board.get(i).get(j) == color &&
+                                this.board.get(i + 1).get(j - 1) == color &&
+                                this.board.get(i + 2).get(j - 2) == color &&
+                                this.board.get(i + 3).get(j - 3) == color));
     }
+
     public boolean tie() {
-        return board.stream().allMatch(row -> row.stream().noneMatch(c -> c == '-'));
+        return board.stream().allMatch(row -> row.stream().noneMatch(c -> c == EMPTY));
     }
     public boolean finished() {
         return finished || this.tie();
--- TP4/linea/src/linea/LineaTest.java
+++ TP4/linea/src/linea/LineaTest.java
@@ -22,40 +22,195 @@ public class LineaTest {
         assertEquals("|X|\n", game.show());
     }
 
-    @Test public void test03PonerFichaFueraDelTablero(){
+    @Test public void test03PonerFichaEnTableroGrande(){
+        Linea game = new Linea(5,5,'C');
+        game.playRedkAt(0);
+        assertEquals("|-----|\n|-----|\n|-----|\n|-----|\n|X----|\n", game.show());
+    }
+
+    @Test public void test04PonerFichaFueraDelTablero(){
         Linea game = new Linea(1,1,'C');
         assertThrows(RuntimeException.class, () -> game.playRedkAt(1));
     }
 
-    @Test public void test04PonerFichaEnColumnaLlena(){
+    @Test public void test05PonerFichaEnColumnaLlena(){
         Linea game = new Linea(2,2,'C');
         game.playRedkAt(0);
         game.playBlueAt(0);
         assertThrows(RuntimeException.class, () -> game.playRedkAt(0));
     }
 
-    @Test public void test05JuegoTerminadoConTableroLleno(){
+    @Test public void test06JuegoTerminadoConTableroLleno(){
         Linea game = new Linea(1,1,'C');
         game.playRedkAt(0);
         assertTrue(game.finished());
     }
 
-    @Test public void test06AzulNoPuedeComenzar(){
+    @Test public void test07AzulNoPuedeComenzar(){
         Linea game = new Linea(1,1,'C');
         assertThrows(RuntimeException.class, () -> game.playBlueAt(0));
     }
 
-    @Test public void test07RojoNoPuedeJugarDosVecesSeguidas(){
+    @Test public void test08RojoNoPuedeJugarDosVecesSeguidas(){
         Linea game = new Linea(2,2,'C');
         game.playRedkAt(0);
         assertThrows(RuntimeException.class, () -> game.playRedkAt(0));
     }
 
-    @Test public void test08AzulNoPuedeJugarDosVecesSeguidas(){
+    @Test public void test09AzulNoPuedeJugarDosVecesSeguidas(){
         Linea game = new Linea(2,2,'C');
         game.playRedkAt(0);
         game.playBlueAt(1);
         assertThrows(RuntimeException.class, () -> game.playBlueAt(0));
     }
+    @Test public void test10VictoriaRojoHorizontalEnA(){
+        Linea game = new Linea(4,4,'A');
+        game.playRedkAt(0);
+        game.playBlueAt(0);
+        game.playRedkAt(1);
+        game.playBlueAt(1);
+        game.playRedkAt(2);
+        game.playBlueAt(2);
+        game.playRedkAt(3);
+        assertTrue(game.finished());
+    }
 
+    @Test public void test11VictoriaRojoVerticalEnA(){
+        Linea game = new Linea(4,4,'A');
+        game.playRedkAt(0);
+        game.playBlueAt(1);
+        game.playRedkAt(0);
+        game.playBlueAt(1);
+        game.playRedkAt(0);
+        game.playBlueAt(1);
+        game.playRedkAt(0);
+        assertTrue(game.finished());
+    }
+
+    @Test public void test12VictoriaRojoDiagonalDerechaEnB(){
+        Linea game = new Linea(4,4,'B');
+        game.playRedkAt(0);
+        game.playBlueAt(1);
+        game.playRedkAt(1);
+        game.playBlueAt(2);
+        game.playRedkAt(2);
+        game.playBlueAt(3);
+        game.playRedkAt(3);
+        game.playBlueAt(3);
+        game.playRedkAt(2);
+        game.playBlueAt(0);
+        game.playRedkAt(3);
+        assertTrue(game.finished());
+    }
+
+    @Test public void test13VictoriaRojoDiagonalIzquierdaEnC(){
+        Linea game = new Linea(5,4,'C');
+        game.playRedkAt(3);
+        game.playBlueAt(2);
+        game.playRedkAt(2);
+        game.playBlueAt(1);
+        game.playRedkAt(1);
+        game.playBlueAt(0);
+        game.playRedkAt(1);
+        game.playBlueAt(0);
+        game.playRedkAt(0);
+        game.playBlueAt(4);
+        game.playRedkAt(0);
+        assertTrue(game.finished());
+    }
+
+    @Test public void test14VictoriaAzulHorizontalEnA(){
+        Linea game = new Linea(5,4,'A');
+        game.playRedkAt(4);
+        game.playBlueAt(0);
+        game.playRedkAt(4);
+        game.playBlueAt(1);
+        game.playRedkAt(4);
+        game.playBlueAt(2);
+        game.playRedkAt(0);
+        game.playBlueAt(3);
+        assertTrue(game.finished());
+    }
+
+    @Test public void test15VictoriaAzulVerticalEnA(){
+        Linea game = new Linea(4,4,'A');
+        game.playRedkAt(0);
+        game.playBlueAt(1);
+        game.playRedkAt(0);
+        game.playBlueAt(1);
+        game.playRedkAt(0);
+        game.playBlueAt(1);
+        game.playRedkAt(0);
+        assertTrue(game.finished());
+    }
+
+    @Test public void test16VictoriaAzulDiagonalDerechaEnB(){
+        Linea game = new Linea(4,4,'B');
+        game.playRedkAt(1);
+        game.playBlueAt(0);
+        game.playRedkAt(2);
+        game.playBlueAt(1);
+        game.playRedkAt(2);
+        game.playBlueAt(2);
+        game.playRedkAt(3);
+        game.playBlueAt(3);
+        game.playRedkAt(3);
+        game.playBlueAt(3);
+        assertTrue(game.finished());
+    }
+
+    @Test public void test17VictoriaAzulDiagonalIzquierdaEnC(){
+        Linea game = new Linea(4,4,'C');
+        game.playRedkAt(0);
+        game.playBlueAt(3);
+        game.playRedkAt(2);
+        game.playBlueAt(2);
+        game.playRedkAt(1);
+        game.playBlueAt(1);
+        game.playRedkAt(0);
+        game.playBlueAt(1);
+        game.playRedkAt(0);
+        game.playBlueAt(0);
+        assertTrue(game.finished());
+    }
+
+    @Test public void test18NOVictoriaHorizontalEnB(){
+        Linea game = new Linea(4,4,'B');
+        game.playRedkAt(0);
+        game.playBlueAt(0);
+        game.playRedkAt(1);
+        game.playBlueAt(1);
+        game.playRedkAt(2);
+        game.playBlueAt(2);
+        game.playRedkAt(3);
+        assertFalse(game.finished());
+    }
+
+    @Test public void test19NOVictoriaVerticalEnB(){
+        Linea game = new Linea(4,4,'B');
+        game.playRedkAt(0);
+        game.playBlueAt(1);
+        game.playRedkAt(0);
+        game.playBlueAt(1);
+        game.playRedkAt(0);
+        game.playBlueAt(1);
+        game.playRedkAt(0);
+        assertFalse(game.finished());
+    }
+
+    @Test public void test20NOVictoriaDiagonalEnA(){
+        Linea game = new Linea(4,4,'A');
+        game.playRedkAt(0);
+        game.playBlueAt(1);
+        game.playRedkAt(1);
+        game.playBlueAt(2);
+        game.playRedkAt(2);
+        game.playBlueAt(3);
+        game.playRedkAt(3);
+        game.playBlueAt(3);
+        game.playRedkAt(2);
+        game.playBlueAt(0);
+        game.playRedkAt(3);
+        assertFalse(game.finished());
+    }
 }
